e5ab9853d8d60f12472063bfc18d3e25e11f6ffe
RyanGabr <ryanalexandregabriel@gmail.com>
2025-07-05 20:34:56 -0300

    Switch app name and add favicon

--- src/screen/Navbar/navbar.tsx
+++ src/screen/Navbar/navbar.tsx
@@ -2,7 +2,7 @@ export function Navbar() {
   return (
     <div className="py-2 px-5 border-b border-border flex items-center justify-between bg-foreground/5 sm:px-20">
       <div>
-        <div className="font-semibold text-rg sm:text-rg md:text-xl lg:text-rg xl:text-xl">Timer</div>
+        <div className="font-semibold text-rg sm:text-rg md:text-xl lg:text-rg xl:text-xl">Focus Pomodoro</div>
       </div>
       <div className="flex items-center gap-5">
         <div className="text-xs font-medium invisible sm:invisible md:visible lg:visible xl:visible">Developed by RyanGabr</div>
